d849f31477209031766e902258256fcac5512952
Joaquin0265 <2022101349@ucss.pe>
2026-06-08 17:01:44 -0500

    Fix: Limpieza de conflictos de merge en pedidos.routes.js

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -5,13 +5,10 @@ const { verificarToken, esAdmin } = require('../middlewares/auth.middleware');
 // Ruta para que el cliente cree su pedido
 router.post('/checkout', verificarToken, pedidosController.crearPedido);
 
-<<<<<<< HEAD
 // Añade esta línea en pedidos.routes.js
 // Ruta para que el cliente vea su historial (Debe estar protegida por el token)
 router.get('/mis-pedidos', verificarToken, pedidosController.obtenerMisPedidos);
 
-=======
->>>>>>> 404a015820dc9690f87fcd68651f120c47f25a44
 // NUEVA: Ruta para que el Admin vea todos los pedidos
 router.get('/admin', verificarToken, esAdmin, pedidosController.listarPedidosAdmin);
 
